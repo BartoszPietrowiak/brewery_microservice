e3d6724444a8ea498e39c11c8665d0bd4709f31e
bpietrowiak <bpietrowiak@consdata.pl>
2023-02-13 13:23:22 +0100

    Project initial

---
+++ src/main/java/com/brewery/miscroservice/MiscroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.miscroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiscroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/miscroservice/MiscroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.miscroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
